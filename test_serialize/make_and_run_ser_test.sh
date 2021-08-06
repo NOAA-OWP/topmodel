6a1c18c4db490aa3b95ab29efab98eba22dab66c
Scott D. Peckham <Scott.Peckham@colorado.edu>
2021-08-06 13:17:26 -0600

    Add files via upload

--- test_serialize/make_and_run_ser_test.sh
+++ test_serialize/make_and_run_ser_test.sh
@@ -4,8 +4,6 @@ if test -f "$FILE"; then
     # If compilation fails, don't want to use old one
     rm "$FILE"
 fi
-# touch run_ser_test
-# mv -f run_ser_test z_trash
 
 gcc ./topmodel_serialize_test.c ./serialize_state.c ../src/bmi_topmodel.c ../src/topmodel.c -o run_ser_test -lm -lmsgpackc 
 ./run_ser_test
