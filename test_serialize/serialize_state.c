dce5fc5c4a51d8b0936b7d757eaecf1350ecb503
Scott D. Peckham <Scott.Peckham@colorado.edu>
2021-08-09 12:04:35 -0600

    Add files via upload

--- test_serialize/serialize_state.c
+++ test_serialize/serialize_state.c
@@ -51,11 +51,14 @@ int serialize(Bmi* model1, const char *ser_file)
   model1->get_state_var_count(model1, &n_state_vars);
     
   FILE *fp = fopen(ser_file, "w+");
-  int i, j;  
+  int i, j, n_bytes;  
   void *ptr_list[ n_state_vars ];
   unsigned int size, sizes[ n_state_vars ];
   int verbose = 1;
-
+  int    i_val;
+  float  f_val;
+  double d_val;
+  
   char **names = NULL;
   names = (char**) malloc (sizeof(char *) * n_state_vars);
   for (i=0; i<n_state_vars; i++){
@@ -162,8 +165,10 @@ int serialize(Bmi* model1, const char *ser_file)
           //---------------------------------------
 		  if (strcmp(type, "string") == 0){
 			  msgpack_pack_str(&pk, size);
-		  } else if (strcmp(type, "FILE") != 0){     
+		  } else if (strcmp(type, "FILE") != 0){
 			  msgpack_pack_array(&pk, size);
+		      // n_bytes = size * sizeof( double );
+			  // msgpack_pack_bin(&pk, n_bytes); 
 		  }
 
           //-----------------------------------------------------
@@ -176,13 +181,18 @@ int serialize(Bmi* model1, const char *ser_file)
 		  //---------------------------------------------
 		  if (strcmp(type, "int") == 0){
 			  for (j=0; j<size; j++){
-				  msgpack_pack_int(&pk, *( ((int *)ptr_list[i]) + j)); }
+			      i_val = *( ((int *)ptr_list[i]) + j);
+				  msgpack_pack_int(&pk, i_val); }
 		  } else if (strcmp(type, "float") == 0){
 			  for (j=0; j<size; j++){
-				  msgpack_pack_float(&pk, *( ((float *)ptr_list[i]) + j)); }
+			      f_val = *( ((float *)ptr_list[i]) + j);
+				  msgpack_pack_float(&pk, f_val); }
 		  } else if (strcmp(type, "double") == 0){
 			  for (j=0; j<size; j++){
-				  msgpack_pack_double(&pk, *( ((double *)ptr_list[i]) + j)); }
+			      d_val = *( ((double *)ptr_list[i]) + j);
+// 			      if ((i > 31) && (i < 45)){
+// 			          printf("  i = %d, j = %d, d_val = %f\n", i, j, d_val); }
+				  msgpack_pack_double(&pk, d_val); }
 		  } else if (strcmp(type, "string") == 0){
 			  // Note:  Need ptr_list[i] without * here.
 			  msgpack_pack_str_body(&pk, ptr_list[i], size);   
@@ -253,13 +263,12 @@ int deserialize_to_state(const char *ser_file, Bmi* model2, int print_obj) {
  
     unsigned int sizes[ n_state_vars ], size;
     model2->get_state_var_sizes(model2, sizes);
-    int j, ival;
-    void *ptr;      //###############
-    float fval;
-    double dval;    //##############
-    
-    char *type;
-    char *sval;
+    int     i_val, *i_arr, j;
+    float   f_val, *f_arr;
+    double  d_val, *d_arr;
+    void    *ptr;
+        
+    char *type, *sval;
     char **types = NULL;
     types = (char**) malloc (sizeof(char *) * n_state_vars);  
     for (int j=0; j<n_state_vars; j++){
@@ -269,7 +278,7 @@ int deserialize_to_state(const char *ser_file, Bmi* model2, int print_obj) {
 
     char inbuffer[BUFFER_SIZE]; 
     FILE *fp = fopen(ser_file, "rb");
-    int i=0;
+    int i = 0;
     size_t off = 0;
     size_t len = 0;
     char unpacked_buffer[UNPACKED_BUFFER_SIZE];
@@ -311,16 +320,15 @@ int deserialize_to_state(const char *ser_file, Bmi* model2, int print_obj) {
 //             model2->set_state_var(model2, ptr, i );
             //--------------------------------------------			
 			if (strcmp(type, "int") == 0){
-				ival = (int)obj.via.i64;
-				model2->set_state_var(model2, &ival, i );
+				i_val = (int)obj.via.i64;
+				model2->set_state_var(model2, &i_val, i );
 			} else if (strcmp(type, "float") == 0){        
-				fval = (float)obj.via.f64;
-				model2->set_state_var(model2, &fval, i );
+				f_val = (float)obj.via.f64;
+				model2->set_state_var(model2, &f_val, i );
 			} else if (strcmp(type, "double") == 0){   
-				dval = (double)obj.via.f64;
-				model2->set_state_var(model2, &dval, i );
+				d_val = (double)obj.via.f64;
+				model2->set_state_var(model2, &d_val, i );
 			} else if (strcmp(type, "string") == 0){
-				// Seems to work for title & subcat
 				model2->set_state_var(model2, &obj, i );
 				//-------------------------------------------
 				// Next 2 lines don't work
@@ -334,48 +342,74 @@ int deserialize_to_state(const char *ser_file, Bmi* model2, int print_obj) {
 				model2->set_state_var(model2, &obj, i );
 			}
         } else{
-            //---------------------------------------------------
-            // Get ptr to first value in array
-            // set_state_var() will use size to set all values
-            // in array and will also typecast.
-            //---------------------------------------------------
-            // This seems to work for var: "dbl_array_test"
-            // but it is just an array, not a ptr to an array.
-            //---------------------------------------------------
-            ptr = obj.via.array.ptr;
-			model2->set_state_var(model2, ptr, i );       
+            //--------------------------------------------------
+            // Copy values from msgpack array object into an
+            // array of the correct type, then pass pointer to
+            // that array to set_state_var(). (This works.)
+            //--------------------------------------------------
+            // Couldn't figure out a clean way to access the
+            // msgpack array elements with pointer math. This
+            // seems to be because it allows mixed types.
+            // Options:  via.u64, via.i64, via.f64, via.str.
+            //--------------------------------------------------
+			if (strcmp(type, "string") == 0){
+				// Works for title & subcat strings in TOPMODEL.
+                ptr = obj.via.array.ptr;
+				model2->set_state_var(model2, ptr, i );
+			} else if (strcmp(type, "int") == 0){
+			    i_arr = (int*) malloc(size * sizeof( int ));
+			    for (int j=0; j<size; j++){
+                     i_arr[j] = obj.via.array.ptr[j].via.i64; }
+				model2->set_state_var(model2, i_arr, i );
+                free(i_arr);
+			} else if (strcmp(type, "float") == 0){
+			    f_arr = (float*) malloc(size * sizeof( float ));
+			    for (int j=0; j<size; j++){
+                     f_arr[j] = obj.via.array.ptr[j].via.f64; }
+				model2->set_state_var(model2, f_arr, i );
+                free(f_arr);
+			} else if (strcmp(type, "double") == 0){
+			    d_arr = (double*) malloc(size * sizeof( double ));
+			    for (int j=0; j<size; j++){
+                     d_arr[j] = obj.via.array.ptr[j].via.f64; }
+				model2->set_state_var(model2, d_arr, i );
+                free(d_arr);
+            }
+            
+            // For testing
+			//printf("### obj.via.array.ptr[0].via.f64 = %f\n", obj.via.array.ptr[0].via.f64);
+			//printf("### obj.via.array.ptr[1].via.f64 = %f\n", obj.via.array.ptr[1].via.f64);
+			//printf("### obj.via.array.ptr[2].via.f64 = %f\n", obj.via.array.ptr[2].via.f64);
+			//printf("\n");
         }
         
-        //---------------------------------------
-        // For now, assume each obj is a scalar
-        //---------------------------------------
-        // model2->set_state_var(model2, ptr, i );        
- 
-        //---------------------------------------------------
-        // Note: If all objects are arrays (even 1 element)
-        //       then may need something like this.
-        //---------------------------------------------------       
-        // ptr = &obj[0];
-        // ptr = &obj.via.array.ptr[0];
-        //##### ptr = obj.via.array.ptr;  // same as prev line?
-        //##### model2->set_state_var(model2, ptr, i );
-        
-        //for (j=0; j<sizes[i]; j++){
-        //     model2->set_state_var(model2, (ptr + j), i );
-        //}              
-        // model2->set_state_var(model2, &obj, i );    //############
-        // model2->set_state_var(model2, &(obj[0]), i );    // DOESN'T WORK
+            //------------------------------------------------------------
+            // Failed attempts to access a single pointer to an array in
+            // the msgpack_array structure, to pass to set_state_var().
+            // Note that typecasting occurs in set_state_var().
+            //------------------------------------------------------------
+// 			if (strcmp(type, "double") == 0){
+// 				ptr = obj.via.array.ptr;
+// 				model2->set_state_var(model2, ptr, i ); }
+//             //-----------------------------------------------
+// 			if (strcmp(type, "double") == 0){
+// 				ptr = &obj;
+// 				model2->set_state_var(model2, ptr, i ); }
+//             //-----------------------------------------------
+// 			if (strcmp(type, "double") == 0){
+// 				ptr = obj.via.array.ptr[0];
+// 				model2->set_state_var(model2, ptr, i ); }
+//             //-----------------------------------------------
+// 			if (strcmp(type, "double") == 0){
+// 				d_val = obj.via.array.ptr[0].via.f64;
+// 				ptr   = &d_val;
+// 				model2->set_state_var(model2, ptr, i ); }
+//             //-----------------------------------------------  
         
         if (print_obj){
             printf("Object no %d:\n", i);
             msgpack_object_print(stdout, obj);
             printf("\n");
-            
-            //if (i == 9){
-            //    printf("obj.via.array.ptr[0].via.u64 = %llu\n", obj.via.array.ptr[0].via.u64);
-            //    printf("obj.via.array.ptr[0].via.i64 = %llu\n", obj.via.array.ptr[0].via.i64);
-            //    printf("obj.via.array.ptr[0].via.f64 = %f\n", obj.via.array.ptr[0].via.f64);
-            //}
               
             //------------------------------
             // This just prints obj again
@@ -426,4 +460,155 @@ int deserialize_to_state(const char *ser_file, Bmi* model2, int print_obj) {
     return 0;
 }
 
+//------------------------------------------------------------------------
+int compare_states(Bmi* model1, Bmi* model2){
+
+  int verbose = 1;
+  int    i, j, match, n_state_vars;
+  int    err_count = 0;
+  int    i_val1, i_val2;
+  float  f_val1, f_val2;
+  double d_val1, d_val2;
+  char   c_val1, c_val2;
+
+  //--------------------------------------
+  // Get total number of state variables
+  //--------------------------------------
+  model1->get_state_var_count(model1, &n_state_vars); 
+
+  //----------------------------------------
+  // Get the state variable internal names
+  //----------------------------------------
+  char **names = NULL;
+  names = (char**) malloc (sizeof(char *) * n_state_vars);
+  for (i=0; i<n_state_vars; i++){
+      names[i] = (char*) malloc (sizeof(char) * BMI_MAX_VAR_NAME);
+  }
+  model1->get_state_var_names(model1, names);
+  
+  //------------------------------------  
+  // Get the state variable data types
+  //------------------------------------
+  char *type;
+  char **types = NULL;
+  types = (char**) malloc (sizeof(char *) * n_state_vars);  
+  for (i=0; i<n_state_vars; i++){
+      types[i] = (char*) malloc (sizeof(char) * BMI_MAX_VAR_NAME);
+  }
+  model1->get_state_var_types(model1, types);
+
+  //-------------------------------  
+  // Get the state variable sizes
+  //-------------------------------
+  unsigned int size, sizes[ n_state_vars ];  
+  model1->get_state_var_sizes(model1, sizes);
+
+  //-------------------------------------        
+  // Get pointers to Model 1 state vars
+  //-------------------------------------
+  void *ptr_list1[ n_state_vars ];
+  model1->get_state_var_ptrs(model1, ptr_list1);
+
+  //-------------------------------------
+  // Get pointers to Model 2 state vars
+  //-------------------------------------  
+  void *ptr_list2[ n_state_vars ];
+  model2->get_state_var_ptrs(model2, ptr_list2);
+
+  //--------------------------------
+  // Loop over all state variables
+  //--------------------------------
+  for (int i=0; i<n_state_vars; i++){
+      match = 1;
+      type  = types[i];
+      size  = sizes[i];
+      // printf("i = %d\n", i);  //##############
+      
+      //-------------------------------
+      // Does type name contain "*" ?
+      //-------------------------------
+      if (strchr(type, '*') != NULL){
+          type[strlen(type)-1] = '\0';  }   // remove last char
+          
+	  //-----------------------------------------------------
+	  // NOTE! Typecast ptr first, then add offset,
+	  //       into array, then dereference the ptr.
+	  //       CORRECT:    *( ((double *)ptr) + j)
+	  //       INCORRECT:  *( (double *)(ptr + j))
+	  //       INCORRECT:  *( (double *)ptr + j)  ??
+	  //       INCORRECT:  *(double *)ptr + j 
+	  //---------------------------------------------
+	  if (strcmp(type, "int") == 0){
+		  for (j=0; j<size; j++){
+			  i_val1 = *( ((int *)ptr_list1[i]) + j);
+			  i_val2 = *( ((int *)ptr_list2[i]) + j);
+			  if (i_val1 != i_val2){
+				  printf("Mismatch: i = %d, j = %d\n", i, j);
+				  printf("i_val1 = %d, i_val2 = %d\n", i_val1, i_val2);
+				  printf("var_name = %s\n\n", names[i]);
+				  match = 0;}
+		  }
+	  } else if (strcmp(type, "float") == 0){
+		  for (j=0; j<size; j++){
+			  f_val1 = *( ((float *)ptr_list1[i]) + j);
+			  f_val2 = *( ((float *)ptr_list2[i]) + j);
+			  if (f_val1 != f_val2){
+				  printf("Mismatch: i = %d, j = %d\n", i, j);
+				  printf("f_val1 = %f, f_val2 = %f\n", f_val1, f_val2);
+				  printf("var_name = %s\n\n", names[i]);
+				  match = 0; }
+		  }
+	  } else if (strcmp(type, "double") == 0){
+		  for (j=0; j<size; j++){
+			  d_val1 = *( ((double *)ptr_list1[i]) + j);
+			  d_val2 = *( ((double *)ptr_list2[i]) + j);
+			  if (d_val1 != d_val2){
+				  printf("Mismatch: i = %d, j = %d\n", i, j);
+				  printf("d_val1 = %f, d_val2 = %f\n", d_val1, d_val2);
+				  printf("var_name = %s\n\n", names[i]);
+				  match = 0; }
+		  }
+	  } else if (strcmp(type, "string") == 0){
+		  if (strcmp(ptr_list1[i], ptr_list2[i]) != 0){
+			  printf("Mismatch: i = %d\n", i);
+			  printf("str1 = %s\n", ptr_list1[i]);
+			  printf("str2 = %s\n", ptr_list2[i]);				  
+			  printf("var_name = %s\n\n", names[i]);
+			  match = 0;
+		  }
+	  } else if (strcmp(type, "FILE") == 0){
+		  j = 0;  // do nothing 
+	  } else{
+		  printf("  Unknown type = %s", types[i] );
+	  }
+
+	  //---------------------------------	
+	  // Did the state variables match?
+	  //---------------------------------
+	  if (match == 0){
+	      err_count++;
+	  } else{
+		  printf("Match: i = %d, name = %s\n", i, names[i]) ;	  
+	  }
+  }
+
+    //--------------------------------   
+    // Free memory for string arrays
+    //--------------------------------
+    if (verbose){ puts("Freeing memory...");}
+    for (i=0; i<n_state_vars; i++){
+        free (names[i]);
+        free (types[i]);
+    }
+    free (names);
+    free (types);
+  
+  printf("Number of state variable differences between\n");
+  printf("   the two model states = %d\n", err_count);
+  printf("Finished comparing model states.\n\n");
+
+  return 0;
+}
+
+//------------------------------------------------------------------------
 
--- test_serialize/topmodel_serialize_test.c
+++ test_serialize/topmodel_serialize_test.c
@@ -16,19 +16,64 @@ called to retrieve the entire model state.
 For Model 2, bmi.initialize() may be called, and then the function
 deserialize_to_state() is called to put it in the same state as Model 1.
 
-For both Models, bmi.update() is then called and results are compared.
+For both Models, bmi.update() is then called several more times and
+results are compared with the compare_states() function.
+
+In C, we cannot infer the type or size of an array, so we need BMI
+functions to get: name, type, size and ptr.
+Recall that array name is a pointer to first element.
+
+Arrays are stored "flattened", so the model will take care of reading
+values into 1D, 2D, 3D arrays.  We should only need the total number
+of array elements.
+
+Later on, ser_file should be an argument to main(), not hard-coded.
 */
 
+//------------------------------------------------------------------------
+int print_some(void *ptr_list[]){
+
+  //------------------------------------------------
+  // Note:  dbl_arr_test is a 3-element test array
+  //------------------------------------------------
+  // NOTE! Typecast ptr first, then add offset,
+  //       into array, then dereference the ptr.
+  //       Careful with order of operations.
+  //------------------------------------------------
+  double a[3];
+  a[0] = *( (double *)ptr_list[56]);
+  a[1] = *( (double *)ptr_list[56] + 1);
+  a[2] = *( (double *)ptr_list[56] + 2);
+  
+  // a[1] = *(double *)(ptr_list[56] + 1);  // This is wrong.
+  // a[2] = *(double *)(ptr_list[56] + 2);
+  
+  printf("ptr_list[6]  = title  = %s", ptr_list[6]);   
+  printf("ptr_list[8]  = dt     = %f\n", *(double *)ptr_list[8]);
+  printf("ptr_list[9]  = nstep  = %d\n", *(int *)ptr_list[9]);
+  printf("ptr_list[32] = Q      = %f\n", *(double *)ptr_list[32]);
+  printf("ptr_list[42] = lnaotb = %f\n", *(double *)ptr_list[42]);
+  printf("ptr_list[50] = cur_tstep = %d\n", *(int *)ptr_list[50]);
+  printf("ptr_list[51] = sump   = %f\n", *(double *)ptr_list[51]);
+  printf("ptr_list[53] = sumq   = %f\n", *(double *)ptr_list[53]);
+  printf("ptr_list[56] = dbl_arr_test = %f, %f, %f\n", a[0], a[1], a[2]);
+  puts("");    // newline is added
+
+  return 0;
+}
+
+//------------------------------------------------------------------------
 int main(void)
 {
   const char *cfg_file = "./data/topmod.run";
-  const char *ser_file = "./model_state.ser";
-  int n_steps1 = 10;  // n_steps for Model1 before serializing
-  int n_steps2 = 50;  // n_steps for Model2 after deserializing  
-  int verbose = 1;
-  int print_obj = 0;  // Set to 1 for upacking details. 
-
-//--------------------------------------------------------------   
+  const char *ser_file = "./model_state.ser";  // make arg later
+  int n_steps1  = 10; // n_steps for Model1 before serializing
+  int n_steps2  = 50; // n_steps for models after deserializing
+  int verbose   = 1;
+  int print_obj = 0;  // Set to 1 to print values after deserializing
+  int n_state_vars;
+    
+  //--------------------------------------------------------------   
   if (verbose){
       puts(""); 
       puts("Allocating memory for TOPMODEL BMI model structure ...");
@@ -36,21 +81,19 @@ int main(void)
   Bmi *model1 = (Bmi *) malloc(sizeof(Bmi));
   Bmi *model2 = (Bmi *) malloc(sizeof(Bmi));
 
-//--------------------------------------------------------------
-  if (verbose){ 
-      puts("Registering TOPMODEL BMI models ...");
-  }
+  //--------------------------------------------------------------
+  if (verbose){ puts("Registering TOPMODEL BMI models ..."); }
+
   register_bmi_topmodel(model1);
   register_bmi_topmodel(model2);
 
-//--------------------------------------------------------------
-  if (verbose){
-      puts("Initializing TOPMODEL BMI models ...");
-  }
+  //--------------------------------------------------------------
+  if (verbose){ puts("Initializing TOPMODEL BMI models ..."); }
+
   model1->initialize(model1, cfg_file);
-  model2->initialize(model2, cfg_file);    //### Needed?
+  model2->initialize(model2, cfg_file);
   
-//--------------------------------------------------------------
+  //--------------------------------------------------------------
   if (verbose){
       puts("Updating TOPMODEL BMI model");
       printf("  n_steps1 = %i \n", n_steps1);
@@ -61,14 +104,12 @@ int main(void)
       model1->update(model1);
   }
 
-//--------------------------------------------------------------  
+  //--------------------------------------------------------------  
   if (verbose){
       puts("Calling BMI.get_state_var_ptrs() on TOPMODEL 1 ...");
   }
 
-  int n_state_vars;
   model1->get_state_var_count(model1, &n_state_vars);  
-  void *ptr_list[ n_state_vars ];
 
   //---------------------------------------------
   // For testing:  All 3 print "8" on my MacPro
@@ -82,56 +123,20 @@ int main(void)
   // See: https://stackoverflow.com/questions/7798383/
   //      array-of-pointers-to-multiple-types-c/7799543
   //--------------------------------------------------------------- 
+  void *ptr_list[ n_state_vars ];
   model1->get_state_var_ptrs(model1, ptr_list);
 
-  //-------------------------------------------------------------
-  // In C, we cannot infer the type or size of an array, so
-  // we need BMI functions to get: name, type, size and ptr.
-  // Recall that array name is a pointer to first element.
-  //------------------------------------------------------------
-  // Arrays are stored "flattened", so the model will take
-  // care of reading values into 1D, 2D, 3D arrays.  We should
-  // only need the total number of array elements.
-  //------------------------------------------------------------
-  if (verbose){
-      printf("ptr_list[6]  = title  = %s", ptr_list[6]);  
-      printf("ptr_list[8]  = dt     = %f\n", *(double *)ptr_list[8]);
-      printf("ptr_list[9]  = nstep  = %d\n", *(int *)ptr_list[9]);
-      printf("ptr_list[32] = Q      = %f\n", *(double *)ptr_list[32]);
-      printf("ptr_list[42] = lnaotb = %f\n", *(double *)ptr_list[42]);
-      printf("ptr_list[50] = cur_tstep = %d\n", *(int *)ptr_list[50]);
-      printf("ptr_list[51] = sump   = %f\n", *(double *)ptr_list[51]);
-      printf("ptr_list[53] = sumq   = %f\n", *(double *)ptr_list[53]);
-  }
-  
-  //----------------------------------------------
-  // NOTE! Typecast ptr first, then add offset,
-  //       into array, then dereference the ptr.
-  //       Careful with order of operations.
-  //----------------------------------------------
-  double a[3];
-  a[0] = *(double *)ptr_list[56];
-  a[1] = *( (double *)ptr_list[56] + 1);
-  a[2] = *( (double *)ptr_list[56] + 2);
-  // a[1] = *(double *)(ptr_list[56] + 1);  // This is wrong.
-  // a[2] = *(double *)(ptr_list[56] + 2);
-  
-  if (verbose){
-      printf("ptr_list[56] = dbl_arr_test = %f, %f, %f\n", a[0], a[1], a[2]);
-      puts("");  // newline is added
-  }
+  if (verbose){ print_some( ptr_list ); }
 
-//--------------------------------------------------------------  
-  if (verbose){
-      puts("Calling serialize() on TOPMODEL 1 ...");
-  }
+  //--------------------------------------------------------------  
+  if (verbose){ puts("Calling serialize() on TOPMODEL 1 ..."); }
 
   //------------------------------------------------
   // Serialize Model1 state and save to:  ser_file
   //------------------------------------------------
   serialize( model1, ser_file );
 
-//--------------------------------------------------------------
+  //--------------------------------------------------------------
   if (verbose){
       puts("Calling deserialize_to_state() on TOPMODEL 2 ...");
   }
@@ -171,29 +176,8 @@ int main(void)
 
   model1->get_state_var_ptrs(model1, ptr_list);
 
-  if (verbose){
-      printf("ptr_list[6]  = title  = %s", ptr_list[6]);   
-      printf("ptr_list[8]  = dt     = %f\n", *(double *)ptr_list[8]);
-      printf("ptr_list[9]  = nstep  = %d\n", *(int *)ptr_list[9]);
-      printf("ptr_list[32] = Q      = %f\n", *(double *)ptr_list[32]);
-      printf("ptr_list[42] = lnaotb = %f\n", *(double *)ptr_list[42]);
-      printf("ptr_list[50] = cur_tstep = %d\n", *(int *)ptr_list[50]);
-      printf("ptr_list[51] = sump   = %f\n", *(double *)ptr_list[51]);
-      printf("ptr_list[53] = sumq   = %f\n", *(double *)ptr_list[53]);
-
-      //----------------------------------------------
-      // NOTE! Typecast ptr first, then add offset,
-      //       into array, then dereference the ptr.
-      //       Careful with order of operations.
-      //----------------------------------------------
-      a[0] = *(double *)ptr_list[56];
-      a[1] = *( (double *)ptr_list[56] + 1);
-      a[2] = *( (double *)ptr_list[56] + 2);
-
-      printf("ptr_list[56] = dbl_arr_test = %f, %f, %f\n", a[0], a[1], a[2]);
-      puts("");    // newline is added
-  }
- 
+  if (verbose){ print_some( ptr_list ); }
+  
   //--------------------------------------------------------------  
   if (verbose){
       puts("Calling BMI.get_state_var_ptrs() on TOPMODEL 2 ...");
@@ -201,43 +185,28 @@ int main(void)
 
   model2->get_state_var_ptrs(model2, ptr_list);
 
-  if (verbose){
-      printf("ptr_list[6]  = title  = %s", ptr_list[6]);   
-      printf("ptr_list[8]  = dt     = %f\n", *(double *)ptr_list[8]);
-      printf("ptr_list[9]  = nstep  = %d\n", *(int *)ptr_list[9]);
-      printf("ptr_list[32] = Q      = %f\n", *(double *)ptr_list[32]);
-      printf("ptr_list[42] = lnaotb = %f\n", *(double *)ptr_list[42]);
-      printf("ptr_list[50] = cur_tstep = %d\n", *(int *)ptr_list[50]);
-      printf("ptr_list[51] = sump   = %f\n", *(double *)ptr_list[51]);
-      printf("ptr_list[53] = sumq   = %f\n", *(double *)ptr_list[53]);
+  if (verbose){ print_some( ptr_list ); }
 
-      //----------------------------------------------
-      // NOTE! Typecast ptr first, then add offset,
-      //       into array, then dereference the ptr.
-      //       Careful with order of operations.
-      //----------------------------------------------
-      a[0] = *(double *)ptr_list[56];
-      a[1] = *( (double *)ptr_list[56] + 1);
-      a[2] = *( (double *)ptr_list[56] + 2);
-
-      printf("ptr_list[56] = dbl_arr_test = %f, %f, %f\n", a[0], a[1], a[2]);
-      puts("");    // newline is added
-  }
   //--------------------------------------------------------------
-  if (verbose){ 
-      puts("Finalizing TOPMODEL BMI Models ...");
-  }
+  if (verbose){ puts("Comparing model states ..."); }
+  
+  compare_states( model1, model2 );
+  
+  //--------------------------------------------------------------
+  if (verbose){ puts("Finalizing TOPMODEL BMI Models ..."); }
+
   model1->finalize(model1);
   model2->finalize(model2);  
- 
-//--------------------------------------------------------------
-// if (verbose){
-//     printf("Freeing memory ... \n");
-// }
-//--------------------------------------------------------------
+
   if (verbose){ 
-      puts("Finished with serialization test.");
-      puts("");
+      puts("Finished with serialization test.\n");
   }
   return 0;
 }
+
+//------------------------------------------------------------------------
+
+
+
+
+
