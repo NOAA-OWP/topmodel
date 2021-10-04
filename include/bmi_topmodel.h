0658a69b50cda55ed74029923cec16a3c3f15944
Scott D. Peckham <Scott.Peckham@colorado.edu>
2021-10-04 14:26:14 -0600

    Add files via upload

--- include/bmi_topmodel.h
+++ include/bmi_topmodel.h
@@ -8,6 +8,19 @@ extern "C" {
 #include "bmi.h"
 #include "topmodel.h"
 
+//--------------------------------------------
+// Used to simplify BMI implementation (SDP)
+//--------------------------------------------
+typedef struct Variable{
+    unsigned int index;
+    char name[80];
+    char type[80];
+    unsigned int size;
+    // char units[80];
+    // char role[80];
+    // bool is_pointer;
+} Variable;
+
 Bmi* register_bmi_topmodel(Bmi *model);
 
 topmodel_model * new_bmi_topmodel();
