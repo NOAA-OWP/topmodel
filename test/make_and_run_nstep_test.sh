1d2cd6229df43a4f1aae3194dc85fb36cdc3e160
madMatchstick <jlgred88@gmail.com>
2021-10-28 11:48:38 -0600

    revert back to BMI2.0

--- test/make_and_run_nstep_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-touch run_bmi_test_nstep
-mv -f run_bmi_test_nstep z_trash
-gcc main_test_nstep.c ../src/bmi_topmodel.c ../src/topmodel.c -o run_bmi_test_nstep -lm
-./run_bmi_test_nstep
