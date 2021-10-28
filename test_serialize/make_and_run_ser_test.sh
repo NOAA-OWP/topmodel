1bf2a3562cbd13ed3fa5d17774d82bfb1695991a
madMatchstick <jlgred88@gmail.com>
2021-10-28 11:48:38 -0600

    revert back to BMI2.0

--- test_serialize/make_and_run_ser_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILE=./run_ser_test
-if test -f "$FILE"; then
-    # If compilation fails, don't want to use old one
-    rm "$FILE"
-fi
-
-gcc ./topmodel_serialize_test.c ./serialize_state.c ../src/bmi_topmodel.c ../src/topmodel.c -o run_ser_test -lm -lmsgpackc 
-./run_ser_test
